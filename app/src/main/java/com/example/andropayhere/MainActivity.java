27660645c26d6c149ad9dd62bb48854074067e4c
wathsala-vishwajith <wathsala-vishwajith@github.com>
2021-05-24 22:40:20 +0530

    Toast

--- app/src/main/java/com/example/andropayhere/MainActivity.java
+++ app/src/main/java/com/example/andropayhere/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.button);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Pressed",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    Button btn1 = findViewById(R.id.button);
+
+
+
 
 }
